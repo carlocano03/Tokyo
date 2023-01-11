57dd70e1bb16e3946c6b0942a11e45c8aeb9c902
Carlo Cano <carlocano03@gmail.com>
2023-01-11 22:19:03 +0800

    Saving beneficiary

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,14 @@ Route::get('admin', [
     // 'uses' => 'Auth\LoginController@showLoginForm'
   ]);
 
+//GET
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/login/get_beneficiary', [HomeController::class, 'get_beneficiary'])->name('getBeneficiary');
+Route::post('/login/delete_beneficiary', [HomeController::class, 'delete_beneficiary'])->name('remove_benefeciaries');
+
+//POST
 Route::post('/login/add_member', [HomeController::class, 'add_member'])->name('add_member');
 Route::post('/login/add_member_con', [HomeController::class, 'add_member_p2'])->name('add_member_con');
+Route::post('/login/add_benefeciaries', [HomeController::class, 'add_benefeciaries'])->name('add_benefeciaries');
+
 
