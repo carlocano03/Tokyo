c2c581026861ec5220010bd79c8544988ccc3ac1
jenarolopez <jenjenlopez37@gmail.com>
2023-01-12 17:09:43 +0800

    footer design

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,86 @@
-<div style="height: 30px; width: 100%;" class="footer">
-  Footer
-</div>
+<footer class="text-center text-lg-start bg-light text-muted d-flex flex-column footer">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div class="d-flex justify-content-center gap-10">
+      <a href="" class="me-4 text-reset link_style_1">
+        <i class="fa fa-facebook-official" aria-hidden="true"></i>
+      </a>
+      <a href="" class="me-4 text-reset link_style_1">
+        <i class="fa fa-twitter-square" aria-hidden="true"></i>
+      </a>
+      <a href="" class="me-4 text-reset link_style_1">
+        <i class="fa fa-google" aria-hidden="true"></i>
+      </a>
+      <a href="" class="me-4 text-reset link_style_1">
+        <i class="fa fa-whatsapp" aria-hidden="true"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3 justify-content-between">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 mx-auto mb-4">
+          <!-- Content -->
+          <h5 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>UP Provident Fund
+          </h5>
+          <p class="mp-input-group__label_footer">All our offices are open to serve you from 8:00 AM until 4:30 PM, Monday to Friday. For your safety, you may also choose to transact with us via email or through the <strong><a class="link_style_1" href="https://member.upprovidentfund.com/">Members Portal</a></strong>.</p>
+          <p class="mp-input-group__label_footer">If you’re applying for a loan, you may email your loan application documents to the appropriate cluster office or upload your application to the <strong><a class="link_style_1" href="https://member.upprovidentfund.com/">Members Portal</a></strong>.</p>
+
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 mx-auto mp-mb4 mp-mt4">
+          <!-- Links -->
+          <p class="mp-input-group__label_footer">If you’re filing a retirement, resignation, death, or separation benefit claim, you may email the requirements to the appropriate cluster office below.</p>
+          <p class="mp-input-group__label_footer">If you have other questions or concerns, feel free to contact us below or message us via the <strong><a class="link_style_1" href="https://www.facebook.com/upprovidentfund">UP Provident Fund Facebook page</a></strong>.</p>
+          <p class="mp-input-group__label_footer">Thank you and stay safe.</p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 d-flex flex-column">
+          <!-- Links -->
+          <h5 class="text-uppercase fw-bold mb-4">Contact</h5>
+          <p class="mp-input-group__label_footer"><i class="fa fa-map-marker w-20" aria-hidden="true"></i>
+            Head Office: A. Roces Sr. Avenue cor. Laurel St. U.P. Diliman, Quezon City</p>
+          <p class="mp-input-group__label_footer">
+            <i class="fa fa-envelope w-20" aria-hidden="true"></i>
+            info@upprovidentfund.com
+          </p>
+          <p class="mp-input-group__label_footer"><i class="fa fa-phone-square w-20" aria-hidden="true"></i> Tel. No.: (02) 8929-3601</p>
+          <p class="mp-input-group__label_footer"><i class="fa fa-facebook-official w-20" aria-hidden="true"></i> <a class="link_style_1">Facebook Page: www.facebook.com/upprovidentfund</a></p>
+
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold link_style_1" href="https://mdbootstrap.com/">THEBOXdevteam.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
