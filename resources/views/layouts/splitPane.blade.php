1aa6c79e9c8d2352db84b32fff18e058b02f0849
carlocano03 <carlocano03@gmail.com>
2023-01-17 14:49:36 +0800

    Fix upload files

--- resources/views/layouts/splitPane.blade.php
+++ resources/views/layouts/splitPane.blade.php
@@ -322,6 +322,7 @@
                                     });
                                     $('.applicationNo').show(200);
                                     $('#application_no').text(reference_no);
+                                    $('#app_no').val(reference_no);
                                     $('#test').val(reference_no);
                                 }
                             }
@@ -501,56 +502,46 @@
 
             }
 
-        } else if (nextValue == 'step-end') {
-            //End
-            var formData = $("#member_forms_3").serialize();
-            var additionalData = {
-                'total_amount': $('#computed_amount').text(),
-                'app_no': reference_no,
-            };
-            formData += '&' + $.param(additionalData);
-            $.ajax({
-                type: 'POST',
-                url: "{{ route('add_member_details') }}",
-                data: formData,
-                success: function(data) {
-                    if (data.success != '') {
-                        Swal.fire({
-                            title: 'Thank you!',
-                            text: "Registration completed",
-                            icon: 'success',
-                            showCancelButton: true,
-                            confirmButtonColor: '#3085d6',
-                            confirmButtonText: 'OK'
-                        }).then((result) => {
-                            if (result.isConfirmed) {
-                                window.open();
-                            }
-                        })
-                        $("#step-2").removeClass('d-flex').addClass("d-none");
-                        $("#step-3").removeClass('d-none').addClass("d-flex");
-                        $("#back").attr('value', 'step-2')
-                        $("#member_forms_con").removeClass('mh-reg-form');
-                        $("#member_forms_3").addClass('mh-reg-form');
-                        $(this).attr('value', 'step-end')
-                        $("#line").removeClass('step-2').addClass('step-3')
-                        $("#registration-title").text(stepTitle[2])
-                        $("#stepper-3").addClass("active")
-                    } else {
-                        Swal.fire({
-                            title: 'Employee No are already used.',
-                            icon: 'error'
-                        });
-
-                        $('#employee_no').focus();
-                    }
-                }
-            });
-
-
-            // $("#btn-submit").click()
         }
         scrollToTop()
+    });
+
+    $(document).on('submit', '#member_forms_3', function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            method: 'POST',
+            url: "{{ route('add_member_details') }}",
+            data: new FormData(this),
+            contentType: false,
+            processData: false,
+            success: function(data) {
+                if (data.success != '') {
+                    Swal.fire({
+                        title: 'Thank you!',
+                        text: "Registration completed",
+                        icon: 'success',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'OK'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.open();
+                        }
+                    })
+                    $("#step-2").removeClass('d-flex').addClass("d-none");
+                    $("#step-3").removeClass('d-none').addClass("d-flex");
+                    $("#back").attr('value', 'step-2')
+                    $("#member_forms_con").removeClass('mh-reg-form');
+                    $("#member_forms_3").addClass('mh-reg-form');
+                    $(this).attr('value', 'step-end')
+                    $("#line").removeClass('step-2').addClass('step-3')
+                    $("#registration-title").text(stepTitle[2])
+                    $("#stepper-3").addClass("active")
+                } 
+            }
+        });
+
     });
 
     $(document).on('click', '#add_dependent', function() {
@@ -723,6 +714,7 @@
         var input2 = $("#monthly_salary").val().replace(/,/g, '');
         var percentage = (input1 / 100) * input2;
         $('#computed_amount').html(percentage);
+        $('#percent_amt').val(percentage);
     });
 
     function scrollToTop() {
