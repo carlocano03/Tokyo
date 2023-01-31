210f8f504694337d2ba67f7a1e11195b28c605fa
Denneb Gomez <92779941+dennebgmz@users.noreply.github.com>
2023-01-31 17:14:22 +0800

    UI/admin settings (#126)
    * ui updates
    
    * added dashboard page
    
    * add mobile toggle
    
    * mobile toggle fix updates
    
    * push conflict
    
    * member dashboard
    
    * pdf member form
    
    * ui changes
    
    * admin ui updates
    
    * member form updates
    
    * member form updates
    
    * delete form spacing
    
    * spacing button layout
    
    * push admin updates
    
    * form updates
    
    * member form updates
    
    * member form pdf
    
    * added some ui updates and added the transaction page, ccontact us liink, member form links
    
    * ui updates member account
    
    * cocolife proxy form
    
    * some ui chores of navigation profile details plus member page ui chores
    
    * fix toggle
    
    * added panel buttons
    
    * settings configuration ui
    
    * change width
    
    * ui updates
    
    * setting pages updates
    
    * settings pages
    
    * cocolife modal ui fix and admin settings label color change
    
    * header spacing
    
    * Revert "UI/admin settings (#123)"
    
    This reverts commit 2a9d47d520f14ff37f34686094166de03b6515a1.
    
    * modal fix
    
    * fix label colrs

--- resources/views/admin/settings-config/employee-classification.blade.php
+++ resources/views/admin/settings-config/employee-classification.blade.php
@@ -2,7 +2,7 @@
 @section('content_body')
 
 <style>
-    /* .card-container {
+  /* .card-container {
     display: flex;
     flex-direction: column;
   }
@@ -291,11 +291,11 @@
   }
 
   .settings-tab label {
-    /* margin-left: 20px; */
+    margin-left: 20px;
     }
 
     .settings-tab a {
-        /* margin-left: 20px; */
+        margin-left: 20px;
     }
 
     .top-label i {
@@ -356,164 +356,164 @@
 <div class="col-12  mp-text-fs-large mp-text-c-accent  dashboard mh-content" style="padding:0px !important;">
 
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-2" style="padding:0px !important; height: 100%; overflow-y:auto; ">
-                <div class="mp-card" style="padding-bottom:150px;">
-                    <div class="settings-tab">
-                        <div class="top-label">
-                            <label>Settings</label>
-                            <i class="fa fa-cog" aria-hidden="true"></i>
-                        </div>
-
-                        <div class="settings-buttons">
-                            <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-                                <li class="options" onclick="location.href='manage-account'">
-                                    <a href="#" class="no-padding">Manage Accounts</a><br>
-                                    <label class="option-info">Allow User to create and manage system user accounts, You also can manage permissions and
-                                        authorizations.
-                                    </label>
-                                </li>
-                                <li class="options" onclick="location.href='campus-management'">
-                                    <a href="#" class="no-padding">Campus Management</a><br>
-                                    <label class="option-info">Allow User to manage respective campus; key, names, and clusters
-                                    </label>
-                                </li>
-                                <li class="options options-active" onclick="location.href='employee-classification'">
-                                    <a href="#" class="no-padding options-a-active">Employee Classification</a><br>
-                                    <label class="option-info options-info-active">Allow User to add and manage employee Classifications
-                                    </label>
-                                </li>
-                                <li class="options " onclick="location.href='college-management'">
-                                    <a href="#" class="no-padding">College / Unit Management</a><br>
-                                    <label class="option-info">Allow User to manage respective College and units
-                                    </label>
-                                </li>
-                                <li class="options" onclick="location.href='department-management'">
-                                    <a href="#" class="no-padding ">Department Management</a><br>
-                                    <label class="option-info ">Allow User to manage respective departments per campuses
-                                    </label>
-                                </li>
-                                <li class="options " onclick="location.href='status-appointment'">
-                                    <a href="#" class="no-padding ">Status and Appointments</a><br>
-                                    <label class="option-info ">Allow User to pre-setup, manage the employee status and appointments.
-                                    </label>
-                                </li>
-                                <li class="options " onclick="location.href='sg-modules'">
-                                    <a href="#" class="no-padding ">SG Modules</a><br>
-                                    <label class="option-info">Allow User to pre-setup salary grade range and assign salary grade category for election
-                                        modules
-                                    </label>
-                                </li>
-                                <li class="options " onclick="location.href='history-logs'">
-                                    <a href="#" class="no-padding ">History Logs</a><br>
-                                    <label class="option-info">Allow User to retrieve and monitor user activity using History logs module.
-                                        modules
-                                    </label>
-                                </li>
-                                <li class="options " onclick="location.href='backup-database'">
-                                    <a href="#" class="no-padding ">Backup Database</a><br>
-                                    <label class="option-info ">Allow User to download and backup system database for documentations and risk management
-                                    </label>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-2" style="padding:0px !important; height: 100%; overflow-y:auto; ">
+        <div class="mp-card" style="padding-bottom:150px;">
+          <div class="settings-tab">
+            <div class="top-label">
+              <label>Settings</label>
+              <i class="fa fa-cog" aria-hidden="true"></i>
             </div>
-            <div class="col-lg-10 mp-mt3 gap-10">
 
-                <div class="mp-card  mp-ph2 mp-pv2">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <div class="top-label">
-                                    <label>Create Account</label>
-                                    <label class="account-info">Allow User to manage respective campus; key, names, and clusters
-                                    </label>
-
-
-                                </div>
-
-                            </div>
-
-                            <div class="col-lg-8">
-                                <div>
-                                    <div class="top-label">
-                                        <label>Data Records</label>
-                                    </div>
-                                    <div class="mp-mt3 table-container" style="height:calc(100%-100px) !important;">
-                                        <table class="members-table" style="height: auto;;" width="100%">
-                                            <thead>
-                                                <tr>
-                                                    <th>
-                                                        <span>Username</span>
-                                                    </th>
-                                                    <th>
-                                                        <span>Password</span>
-                                                    </th>
-                                                    <th>
-                                                        <span>Fullname</span>
-                                                    </th>
-                                                    <th>
-                                                        <span>Campus</span>
-                                                    </th>
-                                                    <th>
-                                                        <span>User Level</span>
-                                                    </th>
-                                                    <th>
-                                                        <span>Status</span>
-                                                    </th>
-                                                    <th>
-                                                        <span>Action</span>
-                                                    </th>
-
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                @for ($i = 0; $i < 100; $i++) <tr>
-                                                    <td>akosirr</td>
-                                                    <td>asd</td>
-                                                    <td>AKO SI RR</td>
-                                                    <td>UP DILIMAN</td>
-                                                    <td>Super admin</td>
-                                                    <td>Pending</td>
-                                                    <td>
-                                                        <button class="up-button-green" style="border-radius: 10px;">
-                                                            view <i class="fa fa-eye" aria-hidden="true"></i>
-                                                        </button>
-
-                                                    </td>
-                                                    </tr>
-
-                                                    @endfor
-                                            </tbody>
-                                        </table>
-
-
-
-                                    </div>
-                                    <div class="records-button" style="transform: scale(0.7);">
-                                        <a class="up-button btn-md   mp-text-center" style="margin-top:3px; width: 160px;" type="submit">
-                                            <span>Print</span>
-                                        </a>
-                                        <br>
-                                        <a class="up-button-green btn-md    mp-text-center" style="margin-top:3px; width: 160px;" type="submit">
-                                            <span>Download</span>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
+            <div class="settings-buttons">
+              <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
+                <li class="options" onclick="location.href='manage-account'">
+                  <a href="#" class="no-padding">Manage Accounts</a><br>
+                  <label class="option-info">Allow User to create and manage system user accounts, You also can manage permissions and
+                    authorizations.
+                  </label>
+                </li>
+                <li class="options" onclick="location.href='campus-management'">
+                  <a href="#" class="no-padding">Campus Management</a><br>
+                  <label class="option-info">Allow User to manage respective campus; key, names, and clusters
+                  </label>
+                </li>
+                <li class="options options-active" onclick="location.href='employee-classification'">
+                  <a href="#" class="no-padding options-a-active">Employee Classification</a><br>
+                  <label class="option-info options-info-active">Allow User to add and manage employee Classifications
+                  </label>
+                </li>
+                <li class="options " onclick="location.href='college-management'">
+                  <a href="#" class="no-padding">College / Unit Management</a><br>
+                  <label class="option-info">Allow User to manage respective College and units
+                  </label>
+                </li>
+                <li class="options" onclick="location.href='department-management'">
+                  <a href="#" class="no-padding ">Department Management</a><br>
+                  <label class="option-info ">Allow User to manage respective departments per campuses
+                  </label>
+                </li>
+                <li class="options " onclick="location.href='status-appointment'">
+                  <a href="#" class="no-padding ">Status and Appointments</a><br>
+                  <label class="option-info ">Allow User to pre-setup, manage the employee status and appointments.
+                  </label>
+                </li>
+                <li class="options " onclick="location.href='sg-modules'">
+                  <a href="#" class="no-padding ">SG Modules</a><br>
+                  <label class="option-info">Allow User to pre-setup salary grade range and assign salary grade category for election
+                    modules
+                  </label>
+                </li>
+                <li class="options " onclick="location.href='history-logs'">
+                  <a href="#" class="no-padding ">History Logs</a><br>
+                  <label class="option-info">Allow User to retrieve and monitor user activity using History logs module.
+                    modules
+                  </label>
+                </li>
+                <li class="options " onclick="location.href='backup-database'">
+                  <a href="#" class="no-padding ">Backup Database</a><br>
+                  <label class="option-info ">Allow User to download and backup system database for documentations and risk management
+                  </label>
+                </li>
+              </ul>
             </div>
+          </div>
 
         </div>
+      </div>
+      <div class="col-lg-10 mp-mt3 gap-10">
+
+        <div class="mp-card  mp-ph2 mp-pv2">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="top-label">
+                  <label>Create Account</label>
+                  <label class="account-info">Allow User to manage respective campus; key, names, and clusters
+                  </label>
+
+
+                </div>
+
+              </div>
+
+              <div class="col-lg-8">
+                <div>
+                  <div class="top-label">
+                    <label>Data Records</label>
+                  </div>
+                  <div class="mp-mt3 table-container" style="height:calc(100%-100px) !important;">
+                    <table class="members-table" style="height: auto;;" width="100%">
+                      <thead>
+                        <tr>
+                          <th>
+                            <span>Username</span>
+                          </th>
+                          <th>
+                            <span>Password</span>
+                          </th>
+                          <th>
+                            <span>Fullname</span>
+                          </th>
+                          <th>
+                            <span>Campus</span>
+                          </th>
+                          <th>
+                            <span>User Level</span>
+                          </th>
+                          <th>
+                            <span>Status</span>
+                          </th>
+                          <th>
+                            <span>Action</span>
+                          </th>
+
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @for ($i = 0; $i < 100; $i++) <tr>
+                          <td>akosirr</td>
+                          <td>asd</td>
+                          <td>AKO SI RR</td>
+                          <td>UP DILIMAN</td>
+                          <td>Super admin</td>
+                          <td>Pending</td>
+                          <td>
+                            <button class="up-button-green" style="border-radius: 10px;">
+                              view <i class="fa fa-eye" aria-hidden="true"></i>
+                            </button>
+
+                          </td>
+                          </tr>
+
+                          @endfor
+                      </tbody>
+                    </table>
+
+
+
+                  </div>
+                  <div class="records-button" style="transform: scale(0.7);">
+                    <a class="up-button btn-md   mp-text-center" style="margin-top:3px; width: 160px;" type="submit">
+                      <span>Print</span>
+                    </a>
+                    <br>
+                    <a class="up-button-green btn-md    mp-text-center" style="margin-top:3px; width: 160px;" type="submit">
+                      <span>Download</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
     </div>
+  </div>
 </div>
 
 
