5fb29991c3d5236142a3e14522bf5da90c2cdc74
Denneb Gomez <92779941+dennebgmz@users.noreply.github.com>
2023-03-06 17:18:32 +0800

    Feature/create election (#189)
    * fix header size and settings tab scroll
    
    * some ui chores
    
    * tooltip design
    
    * commit tooltip
    
    * added proxy form design
    
    * create election side ui
    
    * center pane mobile view fixes
    
    * added asterisk
    
    * added candidate tab

--- resources/views/admin/election/create-election.blade.php
+++ resources/views/admin/election/create-election.blade.php
@@ -62,6 +62,47 @@
     .slider.round:before {
         border-radius: 50%;
     }
+
+    .button-active {
+        background-color: var(--c-accent);
+        color: white;
+
+    }
+
+    .button-menu {
+        font-weight: 600;
+        border-radius: 5px;
+    }
+
+    .candidates {
+        text-align: center;
+        border: solid 1px #c6c6c6;
+        margin: 20px;
+    }
+
+    .candidates label {
+        font-size: 25px;
+        width: 100%;
+        color: white;
+        background-color: var(--c-accent);
+        border: solid 1px black;
+    }
+
+    .candidate-button-container {
+        display: flex;
+        flex-wrap: nowrap;
+        margin: 5px;
+    }
+
+    .candidate-button-container>div {
+        background-color: #f1f1f1;
+        width: 100%;
+        margin: 1px;
+        text-align: center;
+
+
+        font-size: 12px;
+    }
 </style>
 
 
@@ -151,21 +192,28 @@
 
                             </div>
 
-                            <div class="col-lg-5">
+                            <div class="col-lg-4">
                                 <div class="top-label">
                                     <label>Manage Candidates</label>
                                     <br>
-
-                                    <label class="account-info">Allow user to manage Candidates
-                                    </label>
+                                    <button class="mp-input-group__label button-active button-menu">1-15 Category</button>
+                                    <button class="mp-input-group__label button-menu">16 Above Category</button>
                                     {{ csrf_field() }}
                                     <form id="classif_form" class="mh-reg-form form-border-bottom" style="height: calc(100% - 100px) !important;">
 
-                                        <div class="mp-pt3 d-flex gap-10 flex-column mp-pb3 member-form mp-pv2 shadow-inset-1">
+                                        <div class="mp-pt3 d-flex gap-10 flex-column mp-pb3 member-form mp-pv2 shadow-inset-1" style="margin-top: -2px;">
                                             <input type="hidden" id="app_trailNo">
                                             <div class="mp-input-group">
                                                 <label class="mp-input-group__label">Salary Grade</label>
-                                                <label class="mp-input-group__label">102-2912</label>
+                                                <label class="mp-input-group__label">1-15 Category</label>
+
+                                            </div>
+                                            <div class="mp-input-group">
+                                                <label class="mp-input-group__label">Select Candidate Name</label>
+                                                <select class="mp-input-group__input mp-text-field" name="status" id="status" required>
+                                                    <option value="1">Name 1</option>
+                                                    <option value="0">Name 2</option>
+                                                </select>
                                             </div>
                                             <div class="mp-input-group">
                                                 <label class="mp-input-group__label">Select Cluster</label>
@@ -174,45 +222,38 @@
                                                     <option value="0">Cluster 2</option>
                                                 </select>
                                             </div>
+
                                             <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Election Year</label>
+                                                <label class="mp-input-group__label">Select Running Position</label>
+                                                <input style="height: 40px;border: none;" type="file" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
+                                            </div>
+
+                                            <div class="mp-input-group">
+                                                <label class="mp-input-group__label">Select Campus</label>
                                                 <select class="mp-input-group__input mp-text-field" name="status" id="status" required>
-                                                    <option value="1">2020</option>
-                                                    <option value="0">2021</option>
+                                                    <option value="1">Campus 1</option>
+                                                    <option value="0">Campus 2</option>
                                                 </select>
                                             </div>
-
                                             <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Election Date</label>
-                                                <input type="date" id="from" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
-                                            </div>
-
-                                            <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Time Open</label>
-                                                <input type="time" id="from" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
+                                                <label class="mp-input-group__label">Select Candidate Image/Photo *</label>
+                                                <input style="height: 40px;border: none;" type="file" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
                                             </div>
                                             <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Time Close</label>
-                                                <input type="time" id="from" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
-                                            </div>
-
-                                            <div class="mp-input-group">
-                                                <input type="checkbox" class="checkbox-color " style="margin-left:2px;margin-right:3px;" id="terms" name="terms">
-                                                <label class="mp-input-group__label">Open Time / User Access</label>
+                                                <label class="mp-input-group__label">File Attachment</label>
+                                                <input style="height: 40px;border: none;" type="file" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
                                             </div>
 
 
 
 
-                                            <a class="up-button-green btn-md button-animate-right mp-text-center" id="save_class" type="submit">
-                                                <span>OPEN THIS ELECTION</span>
-                                            </a>
+
+
+
                                             <a class="up-button btn-md button-animate-right mp-text-center">
-                                                <span>SAVE DRAFT ELECTION</span>
+                                                <span>ADD CANDIDATE</span>
                                             </a>
-                                            <a class="up-button-grey btn-md button-animate-right mp-text-center">
-                                                <span>CLEAR SETUP</span>
-                                            </a>
+
 
                                             <!-- <button type="submit" class="sss" id="btn-submit">Submit</button> -->
 
@@ -225,91 +266,101 @@
                             </div>
 
 
-                            <div class="col-lg-3">
+                            <div class="col-lg-4">
 
                                 <div class="top-label">
                                     <label>Candidates</label>
+                                    <div class="mp-input-group">
+                                        <label class="mp-input-group__label">PRESIDENTIAL CANDIDATES</label>
 
-
-                                    <label class="account-info">Allow user to create Election
-                                    </label>
-                                    {{ csrf_field() }}
-                                    <form id="classif_form" class="mh-reg-form form-border-bottom" style="height: calc(100% - 100px) !important;">
-
-                                        <div class="mp-pt3 d-flex gap-10 flex-column mp-pb3 member-form mp-pv2 shadow-inset-1">
-                                            <input type="hidden" id="app_trailNo">
-                                            <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Election Reference No:</label>
-                                                <label class="mp-input-group__label">102-2912</label>
-                                            </div>
-                                            <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Select Cluster</label>
-                                                <select class="mp-input-group__input mp-text-field" name="status" id="status" required>
-                                                    <option value="1">Cluster 1</option>
-                                                    <option value="0">Cluster 2</option>
-                                                </select>
-                                            </div>
-                                            <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Election Year</label>
-                                                <select class="mp-input-group__input mp-text-field" name="status" id="status" required>
-                                                    <option value="1">2020</option>
-                                                    <option value="0">2021</option>
-                                                </select>
+                                        <div class="candidates">
+                                            <label>Candidate No 1:</label>
+                                            <h5>Denneb Gomez</h5>
+                                            <div class="profile-img">
+                                                <img src="https://scontent.fmnl4-2.fna.fbcdn.net/v/t39.30808-6/333703943_879550633256042_5999893648977274305_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEvDY9Oe-XZrHs-GDUojjSZgyayc5ndww6DJrJzmd3DDv3w58dPBBxi9TKP4f0RndihehBgfuodgKGh3phfTpJz&_nc_ohc=Rala1y4s5KoAX_E8fm3&_nc_ht=scontent.fmnl4-2.fna&oh=00_AfA9i2OQ2TviYLFewh1RsM4Hl-kAgHga0VpODOgsRh1NtQ&oe=640B1A9D" alt="">
                                             </div>
 
-                                            <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Election Date</label>
-                                                <input type="date" id="from" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
-                                            </div>
+                                            <div class="candidate-button-container">
+                                                <div> <button class="up-button-green edit_coll" style="border-radius: 5px;">
+                                                        VIEW
+                                                        <i style="float:none; margin:0px;" class=" fa fa-eye" aria-hidden="true"></i>
 
-                                            <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Time Open</label>
-                                                <input type="time" id="from" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
-                                            </div>
-                                            <div class="mp-input-group">
-                                                <label class="mp-input-group__label">Time Close</label>
-                                                <input type="time" id="from" class="mp-input-group__input mp-text-field radius-1 border-1 date-input outline" style=" height: 30px;">
-                                            </div>
+                                                    </button>
+                                                </div>
+                                                <div>
+                                                    <button class="up-button-green edit_coll" style="border-radius: 5px;">
+                                                        EDIT
+                                                        <i style="float:none; margin:0px;" class=" fa fa-edit" aria-hidden="true"></i>
 
-                                            <div class="mp-input-group">
-                                                <input type="checkbox" class="checkbox-color " style="margin-left:2px;margin-right:3px;" id="terms" name="terms">
-                                                <label class="mp-input-group__label">Open Time / User Access</label>
+                                                    </button>
+                                                </div>
+                                                <div>
+                                                    <button class="up-button" style="border-radius: 5px;">
+                                                        DELETE
+                                                        <i style="float:none; margin:0px;" class="fa fa-trash" aria-hidden="true"></i>
+
+                                                    </button>
+                                                </div>
+
                                             </div>
 
 
 
 
-                                            <a class="up-button-green btn-md button-animate-right mp-text-center" id="save_class" type="submit">
-                                                <span>OPEN THIS ELECTION</span>
-                                            </a>
-                                            <a class="up-button btn-md button-animate-right mp-text-center">
-                                                <span>SAVE DRAFT ELECTION</span>
-                                            </a>
-                                            <a class="up-button-grey btn-md button-animate-right mp-text-center">
-                                                <span>CLEAR SETUP</span>
-                                            </a>
+                                        </div>
+                                        <div class="candidates">
+                                            <label>Candidate No 2:</label>
+                                            <h5>Denneb Gomez</h5>
+                                            <div class="profile-img">
+                                                <img src="https://scontent.fmnl4-2.fna.fbcdn.net/v/t39.30808-6/333703943_879550633256042_5999893648977274305_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEvDY9Oe-XZrHs-GDUojjSZgyayc5ndww6DJrJzmd3DDv3w58dPBBxi9TKP4f0RndihehBgfuodgKGh3phfTpJz&_nc_ohc=Rala1y4s5KoAX_E8fm3&_nc_ht=scontent.fmnl4-2.fna&oh=00_AfA9i2OQ2TviYLFewh1RsM4Hl-kAgHga0VpODOgsRh1NtQ&oe=640B1A9D" alt="">
+                                            </div>
 
-                                            <!-- <button type="submit" class="sss" id="btn-submit">Submit</button> -->
+                                            <div class="candidate-button-container">
+                                                <div> <button class="up-button-green edit_coll" style="border-radius: 5px;">
+                                                        VIEW
+                                                        <i style="float:none; margin:0px;" class=" fa fa-eye" aria-hidden="true"></i>
+
+                                                    </button>
+                                                </div>
+                                                <div>
+                                                    <button class="up-button-green edit_coll" style="border-radius: 5px;">
+                                                        EDIT
+                                                        <i style="float:none; margin:0px;" class=" fa fa-edit" aria-hidden="true"></i>
+
+                                                    </button>
+                                                </div>
+                                                <div>
+                                                    <button class="up-button" style="border-radius: 5px;">
+                                                        DELETE
+                                                        <i style="float:none; margin:0px;" class="fa fa-trash" aria-hidden="true"></i>
+
+                                                    </button>
+                                                </div>
+
+                                            </div>
+
+
+
 
                                         </div>
 
-                                    </form>
+
+                                    </div>
+
                                 </div>
-
                             </div>
-                        </div>
 
+                        </div>
                     </div>
+
                 </div>
 
             </div>
-
         </div>
     </div>
-</div>
-<script>
-    document.querySelector("input[type=number]")
-        .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1))
-</script>
+    <script>
+        document.querySelector("input[type=number]")
+            .oninput = e => console.log(new Date(e.target.valueAsNumber, 0, 1))
+    </script>
 
-@endsection
+    @endsection
