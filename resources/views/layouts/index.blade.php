dfaa2645f2a98a847e599b9a0dcec4c9ca651d56
jenarolopez <47820660+jenarolopez@users.noreply.github.com>
2023-03-10 00:42:41 +0800

    updates (#198)

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -219,6 +219,30 @@
             background-color: #1a8981 !important;
         }
 
+        @media (max-width:500px) {
+            .cookie-details {
+                width: 100%;
+            }
+            .cookie-body {
+                flex-direction: column;
+            }
+            .cookie-buttons {
+                justify-content: center !important;
+                width: 100%;
+                padding-top: 10px;
+            }
+            .modalContent {
+                height: auto;
+                margin-bottom: 0;
+            }
+            #modalBackDrop {
+                overflow: auto;
+                padding-top: 220px;
+            }
+            .modalFooter>button {
+                font-size: medium;
+            }
+        }
 
         
 
@@ -317,7 +341,7 @@
             </span>
         </div>
         <div class="cookie-body">
-            <div class="w-70 mp-pl2 mp-mt3">By clicking "Accept", you agree to the storing of cookies on your device to enhance site navigation, analyze site usage, and assist in our marketing efforts. <span><a>Privacy and Cookies Policy</a></span></div>
+            <div class="w-70 mp-pl2 mp-mt3 cookie-details">By clicking "Accept", you agree to the storing of cookies on your device to enhance site navigation, analyze site usage, and assist in our marketing efforts. <span><a>Privacy and Cookies Policy</a></span></div>
             <div class="cookie-buttons w-30 d-flex flex-row justify-content-end gap-10 align-items-start mp-pr2">
                 <button class="cookie-accept">Accept</button>
                 <button class="magenta-bg cookie-decline">I do not accept</button>
