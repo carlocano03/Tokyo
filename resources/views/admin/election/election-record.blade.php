ffa466e1943acc605eab39f57d7b55067297688e
Denneb Gomez <92779941+dennebgmz@users.noreply.github.com>
2023-03-28 08:52:00 +0800

    Feature/modal hrdo (#231)
    * remove panget na election module
    
    * added new election ui
    
    * setup election modal and setup candidates modal ui
    
    * election analytics design:
    
    * fix qa ui
    
    * election modal updates
    
    * fix modal timeout
    
    * ui chores
    
    * members module ui
    
    * member modules ui
    
    * all member modules ui
    
    * remove lorem ipsum
    
    * added error swal if no  changes made
    
    * push modal on hrdo
    
    * delete empty style
    
    * change position

--- resources/views/admin/election/election-record.blade.php
+++ resources/views/admin/election/election-record.blade.php
@@ -729,18 +729,15 @@
                             <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
                                 <li class="options options-active" onclick="location.href='election-record'">
                                     <a href="#" class="no-padding options-a-active">Election Records</a><br>
-                                    <label class="option-info options-info-active">Allow User to manage election records.
-                                    </label>
+
                                 </li>
                                 <li class="options" onclick="location.href='create-election'">
                                     <a href="#" class="no-padding">Create New Election</a><br>
-                                    <label class="option-info">Allow User to create new election
-                                    </label>
+
                                 </li>
                                 <li class="options" onclick="location.href='election-analytics'">
                                     <a href="#" class="no-padding">Election Day Analytics</a><br>
-                                    <label class="option-info">Allow User to view election day analytics
-                                    </label>
+
                                 </li>
 
                             </ul>
