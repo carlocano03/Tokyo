ad3f0059ab97cbab6e10a6347cf3cface1f8f951
dennebgmz <denneb.prosolutions@gmail.com>
2023-04-02 17:15:42 +0800

    push axa form updates

--- resources/views/pdf/axa_form.blade.php
+++ resources/views/pdf/axa_form.blade.php
@@ -25,7 +25,7 @@
         }
 
         .col-1 {
-            margin-top: 124px;
+            margin-top: 128px;
         }
 
         .col-1 .firstname_value {
@@ -38,7 +38,7 @@
         }
 
         .col-2 {
-            margin-top: 27px;
+            margin-top: 45px;
         }
 
         .col-3 {
@@ -116,49 +116,80 @@
             margin-top: 4px;
 
         }
+
+        .absolute {
+            position: absolute;
+        }
+
+        .gg-check {
+            box-sizing: border-box;
+            position: relative;
+            display: block;
+            transform: scale(var(--ggs, 1));
+            width: 22px;
+            height: 22px;
+            border: 2px solid transparent;
+            border-radius: 100px
+        }
+
+        .gg-check::after {
+            content: "";
+            display: block;
+            box-sizing: border-box;
+            position: absolute;
+            left: 3px;
+            top: -1px;
+            width: 6px;
+            height: 10px;
+            border-width: 0 2px 2px 0;
+            border-style: solid;
+            transform-origin: bottom left;
+            transform: rotate(45deg)
+        }
     </style>
 </head>
 
 <body>
+
     <div class="pdf-container">
         <img src="{{ public_path().'/assets/pdf/axa-form-base.jpg' }}" width="100%" alt="UPPFI">
     </div>
     <div class="value-container">
         <div class="col-1">
-            <label class="lastname_value" style="font-size:10px;">{{$member->lastname}}</label>
-            <label class="firstname_value" style="margin-left:210px; font-size:10px;">{{$member->firstname}}</label>
-            <label class="middlename_value" style="margin-left:200px; font-size:10px;">{{$member->middlename}}</label>
+            <label class="lastname_value absolute " style="font-size:10px;">{{$member->lastname}}</label>
+            <label class="firstname_value absolute " style="margin-left:240px; font-size:10px;">{{$member->firstname}}</label>
+            <label class="middlename_value absolute " style="margin-left:480px; font-size:10px;">{{$member->middlename}} </label>
         </div>
         <div class="col-2">
-            <label class="sex_value" style="font-size:10px;">{{$member->gender}}</label>
-            <label class="civilstatus_value" style="margin-left:30px; font-size:10px;">{{$member->civilstatus}}</label>
-            <label class="dob_value" style="margin-left:35px; font-size:10px;">{{ date('m/d/Y', strtotime($member->date_birth)) }}</label>
-            <label class="age" style="margin-left:110px; font-size:10px;">{{ \Carbon\Carbon::parse($member->date_birth)->age }}</label>
-            <label class="pob_value" style="margin-left:50px; font-size:10px;">{{$member->place_birth}}</label>
-            <label class="nationality_value" style="margin-left:95px; font-size:10px;">{{$member->citizenship}}</label>
+            <label class="sex_value absolute " style="font-size:10px;">{{$member->gender}}</label>
+            <label class="civilstatus_value absolute " style="margin-left:65px; font-size:10px;">{{$member->civilstatus}}</label>
+            <label class="dob_value absolute " style="margin-left:135px; font-size:10px;">{{ date('m/d/Y', strtotime($member->date_birth)) }}</label>
+            <label class="age absolute " style="margin-left:290px; font-size:10px;">{{ \Carbon\Carbon::parse($member->date_birth)->age }}</label>
+            <label class="pob_value absolute " style="margin-left:355px; font-size:10px;">{{$member->place_birth}}</label>
+            <label class="nationality_value absolute " style="margin-left:525px; font-size:10px;">{{$member->citizenship}}</label>
 
 
         </div>
-        <div class="col-3">
-            <label class="address_value" style="font-size:10px;">{{$member->bldg_street}} {{ $member->barangay}} {{ $member->municipality}} {{ $member->province}} {{$member->zipcode}}</label>
+        <div class="col-3" style="margin-top:42px;">
+            <label class="address_value absolute " style="font-size:10px;">{{$member->bldg_street}} {{ $member->barangay}} {{ $member->municipality}} {{ $member->province}} {{$member->zipcode}}</label>
 
         </div>
-        <div class="col-4">
-            <label class="email_value" style="font-size:10px;">{{$member->email}}</label>
-            <label class="contactNo_value" style="margin-left:100px; font-size:10px;">{{$member->contact_no}}</label>
-            <label class="mobileNo_value" style="margin-left:185px; font-size:10px;">{{$member->contact_no}}</label>
+        <div class="col-4" style="margin-top:42px;">
+            <label class="email_value absolute " style="font-size:10px;">{{$member->email}}</label>
+            <label class="contactNo_value absolute " style="margin-left:245px; font-size:10px;">{{$member->contact_no}}</label>
+            <label class="mobileNo_value absolute " style="margin-left:485px; font-size:10px;">{{$member->contact_no}}</label>
         </div>
-        <div class="col-5">
-            <label class="lastname_value" style="font-size:10px;">{{$member->emp_union_assoc}}</label>
-            <label class="firstname_value" style="margin-left:138px; font-size:10px;">{{$member->occupation}}</label>
-            <label class="middlename_value" style="margin-left:80px; font-size:10px;">{{$member->tin_no}}</label>
-            <label class="middlename_value" style="margin-left:120px; font-size:10px;">{{$member->sss_gsis}}</label>
+        <div class="col-5" style="margin-top:45px;">
+            <label class="lastname_value absolute " style="font-size:10px;">{{$member->emp_union_assoc}}</label>
+            <label class="firstname_value absolute " style="margin-left:245px; font-size:10px;">{{$member->occupation}}</label>
+            <label class="middlename_value absolute " style="margin-left:400px; font-size:10px;">{{$member->tin_no}}</label>
+            <label class="middlename_value absolute " style="margin-left:560px; font-size:10px;">{{$member->sss_gsis}}</label>
         </div>
-        <div class="col-6">
-            <label class="" style="font-size:10px;">{{$member->spouse_name}}</label>
-            <label class="" style="margin-left:265px; font-size:10px;">{{$member->maiden_name}}</label>
+        <div class="col-6" style="margin-top:42px;">
+            <label class=" absolute " style="font-size:10px;">{{$member->spouse_name}}</label>
+            <label class=" absolute " style="margin-left:362px; font-size:10px;">{{$member->maiden_name}}</label>
         </div>
-        <div class="col-7">
+        <div class="col-7" style="margin-top:24px;">
 
             <span class="{{ $member->monthly_salary < 25000 ? 'dot' : '' }}" style="margin-left:103px;  "></span>
             <span class="{{ $member->monthly_salary > 25000 && $member->monthly_salary <= 50000 ? 'dot' : '' }}" style="margin-left:192px;  "></span>
@@ -171,70 +202,69 @@
             <input type="radio" style="margin-left:528px;" {{ $member->insuted_type == 'INSURED' ? 'checked' : '' }}>
             <input type="radio" style="margin-left:608px;" {{ $member->insuted_type == 'DEPENDENT' ? 'checked' : '' }}> -->
         </div>
-        <div class="col-8">
-            <label class="" style="font-size:10px;">{{$member->last_name}}</label>
-            <label class="" style="margin-left:205px; font-size:10px;">{{$member->first_name}}</label>
-            <label class="" style="margin-left:178px; font-size:10px;">{{$member->middle_name}}</label>
+        <div class="col-8" style="margin-top:68px;">
+            <label class=" absolute " style="font-size:10px;">{{$member->last_name}}</label>
+            <label class=" absolute " style="margin-left:245px; font-size:10px;">{{$member->first_name}}</label>
+            <label class=" absolute " style="margin-left:478px; font-size:10px;">{{$member->middle_name}}</label>
         </div>
-        <div class="col-9">
-            <label class="" style="font-size:10px;">{{$member->relationship_tomember}}</label>
-            <label class="" style="margin-left:180px; font-size:10px;">{{$member->contact_no}}</label>
-            <label class="" style="margin-left:170px; font-size:10px;">{{$member->email_add}}</label>
+        <div class="col-9" style="margin-top:42px;">
+            <label class=" absolute " style="font-size:10px;">{{$axa_info->relationship_tomember}}</label>
+            <label class=" absolute " style="margin-left:240px; font-size:10px;">{{$member->contact_no}}</label>
+            <label class=" absolute " style="margin-left:480px; font-size:10px;">{{$member->email_add}}</label>
         </div>
 
-        <div class="col-10" style="font-size:8px;  margin-top:60px;">
+        <div class="col-10" style="font-size:8px;  margin-top:65px;">
             @foreach($benificiary as $key => $ben)
-            <div class="col" style="margin-top:16px;">
-                <label class="b_lastname_value">
-                {{ $ben->last_name }}
+            <div class="col" style="margin-top:26px;">
+                <label class=" absolute b_lastname_value">
+                    {{ $ben->last_name }}
                 </label>
-                <label class="b_firstname_value" style="margin-left:75px;">
-                {{ $ben->first_name }}
+                <label class=" absolute b_firstname_value" style="margin-left:110px;">
+                    {{ $ben->first_name }}
                 </label>
-                <label class="b_initials_value" style="margin-left:86px;">
-                {{ $ben->middle_name }}
+                <label class=" absolute b_initials_value" style="margin-left:218px;">
+                    {{ $ben->middle_name }}
                 </label>
-                <label class="b_dob_value" style="margin-left:15px;">
-                {{ $ben->date_of_bday }}
+                <label class=" absolute b_dob_value" style="margin-left:258px;">
+                    {{ $ben->date_of_bday }}
                 </label>
-                <label class="b_relationship_value" style="margin-left:35px;">
-                {{ $ben->ben_relationship }}
+                <label class=" absolute b_relationship_value" style="margin-left:325px;">
+                    {{ $ben->ben_relationship }}
                 </label>
-                <label class="b_benifit_value" style="margin-left:70px;">
+                <label class=" absolute b_benifit_value" style="margin-left:435px;">
+                    {{ $member->percentage }} %
+                </label>
+                <label class=" absolute b_primary_value" style="margin-left:510px;">
+                    <span class="{{ $ben->insured_type == 'PRIMARY' ? 'gg-check' : '' }}"></span>
+                </label>
+                <label class=" absolute b_secondary_value" style="margin-left:569px;">
+                    <span class="{{ $ben->insured_type == 'SECONDARY' ? 'gg-check' : '' }}"></span>
+                </label>
+                <label class=" absolute b_revocable_value" style="margin-left:625px;">
+                    <span class="{{ $ben->according_rights == 'REVOCABLE' ? 'gg-check' : '' }}"></span>
+                </label>
+                <label class=" absolute b_irrevocable_value" style="margin-left:675px;">
+                    <span class="{{ $ben->according_rights == 'IRREVOCABLE' ? 'gg-check' : '' }}"></span>
+                </label>
 
-                </label>
-                <label class="b_primary_value" style="margin-left:35px;">
-
-                </label>
-                <label class="b_secondary_value" style="margin-left:20px;">
-
-                </label>
-                <label class="b_revocable_value" style="margin-left:20px;">
-
-                </label>
-                <label class="b_irrevocable_value" style="margin-left:20px;">
-
-                </label>
-
-        </div>
-        @endforeach
-    </div>
-
-    <div class="signature">
-        <div class="col-1">
-            <!-- <img src="{{ asset($member->signature) }}" width="50px" height="50px" alt="UPPFI"> -->
-            <img src="{{ public_path($member->signature)}}" width="50px" height="50px" alt="UPPFI">
-            <br>
-            <label>{{ $member->sign }}</label>
+            </div>
+            @endforeach
         </div>
 
-        <div class="col-2">
-            <!-- <img src="{{ public_path().'/assets/images/uppfi-logo.png' }}" width="50px" height="50px" alt="UPPFI">
+        <div class="signature absolute">
+            <div class="col-1 absolute" style="margin-top:40px; margin-left:110px;">
+                <!-- <img src="{{ asset($member->signature) }}" width="50px" height="50px" alt="UPPFI"> -->
+                <img src="{{ public_path($member->signature) }}" class="absolute" style="margin-top:110px; " width="50px" height="50px" alt="UPPFI">
+                <br>
+                <label class="absolute" style="margin-top:130px;">{{ $member->sign }}</label>
+            </div>
+
+            <div class="col-2 absolute" style="margin-top:0px;">
+                <!-- <img src="{{ public_path().'/assets/images/uppfi-logo.png' }}" width="50px" height="50px" alt="UPPFI">
                 <br> -->
-            <label>{{ date('m/d/Y', strtotime($member->time_stamp_datesigned)) }}</label>
+                <label class="absolute" style="margin-left:212px; margin-top:190px;">{{ date('m/d/Y', strtotime($member->time_stamp_datesigned)) }}</label>
+            </div>
         </div>
-    </div>
-    </div>
 
 
 
