47f79c43d807bcee3f63058d01ae6faae02638e0
Denneb Gomez <92779941+dennebgmz@users.noreply.github.com>
2023-05-17 14:07:23 +0800

    Feature/members ledger backend (#329)
    * Fix NCR locations
    
    * Fix application movement
    
    * Update application movement
    
    * Fix membership registration
    
    * feature/save-as-draft
    
    * fix gcash ending:
    
    * fix axa layout
    
    * fix axa form design
    
    * additional bene layout fixings
    
    * push axa form updates
    
    * push some updates
    
    * fix reset buttons on settings config
    
    * uat 3 form fixes and value of step 5
    
    * added create election backend
    
    * election features and backend
    
    * some to follow updates
    
    * add candidates ui functions
    
    * election updates
    
    * fix duplicate errors on settings tab
    
    * added election validation and election fixes
    
    * some election updates
    
    * fix file uploading on election add candidates
    
    * added sg15 ang sg16 backend and validations
    
    * test
    
    * fix settings
    
    * ui registration fix
    
    * election updates
    
    * fix/ui tabbing
    
    * transaction uis
    
    * load module ui
    
    * uat 4 step 4 and 5 updates
    
    * some spacings
    
    * added benifit blade
    
    * load application list ui and generate pdf design
    
    * backend retrival of loan details from v1
    
    * some backend updates
    
    * member details backend
    
    * added change status  on admin member side,manage beneficiaries backends
    
    * manage member details backend
    
    * password reset updates
    
    * loan payments list
    
    * download pdf for loans payment + multi
    
    * generate pdf on members ledger

--- routes/web.php
+++ routes/web.php
@@ -220,6 +220,8 @@ Route::get('/axaform/{id}', [PDFController::class, 'axaForm']);
 Route::get('/downloadFormProxy', [PDFController::class, 'downloadForm'])->name('download_form');
 Route::get('/downloadCoco', [PDFController::class, 'downloadCoco'])->name('download_coco');
 Route::get('/downloadProxy', [PDFController::class, 'downloadProxy'])->name('download_proxy');
+Route::get('/admin/generate/soa/{id}', 'AdminController@generatesoa');
+
 
 Route::get('/admin/loan/loan-details/generate-payslip', [PDFController::class, 'generatePayslip'])->name('generate-payslip');
 
