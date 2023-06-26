395a415bcf26977115cddca6ad1e3425e01980ea
dennebgmz <denneb.prosolutions@gmail.com>
2023-06-26 20:46:15 +0800

    view not draft member loan applications

--- resources/views/member/loan_application/index.blade.php
+++ resources/views/member/loan_application/index.blade.php
@@ -492,7 +492,7 @@
                 console.log(data);
 
                 $('#total_processing').text(data.total_processing > 0 ? data.total_processing : "0");
-                $('#total_new').text(data.total_processing > 0 ? data.total_processing : "0");
+                $('#total_new').text(data.total_new > 0 ? data.total_new : "0");
                 $('#total_confirmed').text(data.total_confirmed > 0 ? data.total_confirmed : "0");
                 $('#total_for_review').text(data.total_for_review > 0 ? data.total_for_review : "0");
                 $('#total_approved').text(data.total_approved > 0 ? data.total_approved : "0");
